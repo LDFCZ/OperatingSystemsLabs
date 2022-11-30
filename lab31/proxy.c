e00ee7dcc2bc8dd344a0aedcbeb0158b59b7a575
LDFCZ <v.lopatkin1@g.nsu.ru>
2022-11-30 16:44:42 +0700

    WIP

--- lab31/proxy.c
+++ lab31/proxy.c
@@ -300,7 +300,7 @@ int main(int argc, char *argv[]) {
                 size_t len = 200;
                 char* url_buffer = (char *)malloc(sizeof(char) * len);
                 //int read_bytes = read(clients[client_i], &url_buffer, ADDRESS_BUF_SIZE);
-                int read_bytes = getline(&url_buffer, &len, clients[client_i]);
+                int read_bytes = getline(&url_buffer, &len, fdopen(clients[client_i], "r"));
                 //url_buffer[strlen(url_buffer) - 1] = '\0';
                 if (read_bytes) {
                     url_buffer[read_bytes] = '\0';
